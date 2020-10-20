0ef1c1d755f63ae3b9956c4629cf7331d5227f7b
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2020-10-20 22:57:43 +0100

    add game flow comments

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,21 @@
 // insert game here...
+
+/* this is how the game should flow:
+
+  user presses button to start game
+
+  user picks rock/paper/scissors 
+
+  using Math.random to generate random number b/w 0 and 1
+  
+  if or switch-statement --> 
+    if math.random is up to 0.33 - rock, 
+    if b/w 0.34-0.66 - paper, 
+    if 0.67 or higher - scissors
+
+  alert or add div/text of some kind to say "you win" or "you lose"
+
+  add score keeping mechanism... maybe best of 3 or first to 5 wins?
+
+  add functionality to add their name to a leader board?
+*/
