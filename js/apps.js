e423a5ee82914e114da58b4ce57ad173a22de43c
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2020-10-28 20:26:07 +0000

    amend results section and add comments re alt solution
    trying to figure out an alternative solution that would run and read a bit smoother than what I currently have

--- js/apps.js
+++ js/apps.js
@@ -28,56 +28,88 @@ document.addEventListener('DOMContentLoaded', function() {
   var rock = document.getElementById('rock');
   var paper = document.getElementById('paper');
   var scissors = document.getElementById('scissors');
+
+  var picksSection = document.getElementById("picks");
   var resultsSection = document.getElementById("results");
+  
 
   function game(userSelection) {
+    picksSection.innerHTML = ""
     resultsSection.innerHTML = "";
+
     let compSelection = '';
-    let randomNum = Math.floor(Math.random() * 3) + 1;
+    let randomNum = Math.floor(Math.random() * 3);
 
     // this if statement will decide what option the computer picked
     switch (randomNum) {
-      case 1: 
+      case 0: 
         compSelection = 'Rock';
         break;
-      case 2: 
+      case 1: 
         compSelection = 'Paper';
         break;
-      case 3: 
+      case 2: 
         compSelection = 'Scissors';
         break;
     }
-    
 
-    //  this if statement will compare the userSelection with the compSelection (also this is the rough draft but there will be a nicer way to do this)
+    picksSection.innerHTML += `
+      <h2>You picked ${userSelection} and the computer picked ${compSelection}.</h2>
+    `;    
+
+    //  this if statement will compare the userSelection with the compSelection (also this is the rough draft and i will find a nicer way to do this)
     if (userSelection === compSelection) {
       resultsSection.innerHTML += `
-      <h2>"You picked ${userSelection} and the computer picked ${compSelection}. It's a tie!"</h2>
+      <h2>It's a tie!</h2>
       `;    
     } else if ((userSelection === 'Rock' && compSelection === 'Paper') || (userSelection === 'Paper' && compSelection === 'Scissors') || (userSelection === 'Scissors' && compSelection === 'Rock')) {
       resultsSection.innerHTML += `
-      <h2>"You picked ${userSelection} and the computer picked ${compSelection}. You lose!"</h2>
+      <h2>You lose!</h2>
       `;
     } else if ((userSelection === 'Paper' && compSelection === 'Rock') || (userSelection === 'Scissors' && compSelection === 'Paper') || (userSelection === 'Rock' && compSelection === 'Scissors')) {
       resultsSection.innerHTML += `
-      <h2>"You picked ${userSelection} and the computer picked ${compSelection}. You win!"</h2>
+      <h2>You win!</h2>
       `;
     }
+
   }
 
   rock.addEventListener("click", function () {
     game('Rock'); 
-    // is this enough for the game to run?
   });
 
   paper.addEventListener("click", function () {
     game('Paper'); 
-    // is this enough for the game to run?
   });
 
   scissors.addEventListener("click", function () {
     game('Scissors'); 
-    // is this enough for the game to run?
   });
+
+
+  
+  /* Alternate solution
+
+    let compSelection = Math.floor(Math.random() * 3);
+
+    // 0 = rock
+    // 1 = paper
+    // 2 = scissors
+
+    if userSelection is 0 and compSelection is 1 comp wins...
+
+    user needs to get one higher than comp to win.
+
+    so if (userSelection === compSelection + 1) user wins
+    and if (userSelection === compSelection - 1) comp wins
+
+    OR maybe
+
+    if (userSelection > compSelection) user wins
+    and if (userSelection < compSelection) comp wins
+
+    but these don't account for whenever compselection is scissors or when userselection is rock... hmmmm
+  
+  */
 
 });
