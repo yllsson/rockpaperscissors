1ce5405dfcd86d15d9e7d8cbc17e0530366c8c7c
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2020-10-28 18:38:58 +0000

    fix game - the game works!!
    Wohoo!

--- js/apps.js
+++ js/apps.js
@@ -22,65 +22,62 @@
 
 
 
-// document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
-//   resultsSection.innerHTML += `
+  // grabbing all the html elements and storing into variables
+  var rock = document.getElementById('rock');
+  var paper = document.getElementById('paper');
+  var scissors = document.getElementById('scissors');
+  var resultsSection = document.getElementById("results");
 
-//   `;
+  function game(userSelection) {
+    resultsSection.innerHTML = "";
+    let compSelection = '';
+    let randomNum = Math.floor(Math.random() * 3) + 1;
 
-// });
+    // this if statement will decide what option the computer picked
+    switch (randomNum) {
+      case 1: 
+        compSelection = 'Rock';
+        break;
+      case 2: 
+        compSelection = 'Paper';
+        break;
+      case 3: 
+        compSelection = 'Scissors';
+        break;
+    }
+    
 
-// grabbing all the html elements and storing into variables
-var rock = document.getElementById('rock');
-var paper = document.getElementById('paper');
-var scissors = document.getElementById('scissors');
-var resultsSection = document.getElementById("results");
-
-const game = function(userSelection) {
-  let compSelection = '';
-  let randomNum = Math.floor(Math.random() * 3) + 1;
-
-  // this if statement will decide what option the computer picked
-  switch (randomNum) {
-    case 1: 
-      compSelection = 'Rock';
-      break;
-    case 2: 
-      compSelection = 'Paper';
-      break;
-    case 3: 
-      compSelection = 'Scissors';
-      break;
+    //  this if statement will compare the userSelection with the compSelection (also this is the rough draft but there will be a nicer way to do this)
+    if (userSelection === compSelection) {
+      resultsSection.innerHTML += `
+      <h2>"You picked ${userSelection} and the computer picked ${compSelection}. It's a tie!"</h2>
+      `;    
+    } else if ((userSelection === 'Rock' && compSelection === 'Paper') || (userSelection === 'Paper' && compSelection === 'Scissors') || (userSelection === 'Scissors' && compSelection === 'Rock')) {
+      resultsSection.innerHTML += `
+      <h2>"You picked ${userSelection} and the computer picked ${compSelection}. You lose!"</h2>
+      `;
+    } else if ((userSelection === 'Paper' && compSelection === 'Rock') || (userSelection === 'Scissors' && compSelection === 'Paper') || (userSelection === 'Rock' && compSelection === 'Scissors')) {
+      resultsSection.innerHTML += `
+      <h2>"You picked ${userSelection} and the computer picked ${compSelection}. You win!"</h2>
+      `;
+    }
   }
-  
 
-  //  this if statement will compare the userSelection with the compSelection (also this is the rough draft but there will be a nicer way to do this)
-  if (userSelection === compSelection) {
-    resultsSection.innerHTML += `
-    <h2>"You picked ${userSelection} and the computer picked ${compSelection}. It's a tie!"</h2>
-    `;    
-  } else if ((userSelection === 'Rock' && compSelection === 'Paper') || (userSelection === 'Paper' && compSelection === 'Scissors') || (userSelection === 'Scissors' && compSelection === 'Rock')) {
-    resultsSection.innerHTML += `
-    <h2>"You picked ${userSelection} and the computer picked ${compSelection}. You lose!"</h2>
-    `;
-  } else if ((userSelection === 'Paper' && compSelection === 'Rock') || (userSelection === 'Scissors' && compSelection === 'Paper') || (userSelection === 'Rock' && compSelection === 'Scissors')) {
-    resultsSection.innerHTML += `
-    <h2>"You picked ${userSelection} and the computer picked ${compSelection}. You win!"</h2>
-    `;
-  }
-}
+  rock.addEventListener("click", function () {
+    game('Rock'); 
+    // is this enough for the game to run?
+  });
 
-rock.addEventListener("click", function () {
-  game('Rock'); 
-  // is this enough for the game to run?
-});
+  paper.addEventListener("click", function () {
+    game('Paper'); 
+    // is this enough for the game to run?
+  });
 
-paper.addEventListener("click", function () {
-  game('Paper'); 
-  // is this enough for the game to run?
-});
+  scissors.addEventListener("click", function () {
+    game('Scissors'); 
+    // is this enough for the game to run?
+  });
 
-scissors.addEventListener("click", function () {
-  game('Scissors'); 
-  // is this enough for the game to run?
 });
